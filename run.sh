6845a6a807559f47be83abeb5ad2c14ea5adddf4
Ray Ozzie <ray@ozzie.net>
2021-04-26 06:43:09 -0400

    deploy

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-# Note that by placing the body of this procedure into 
+# Note that by placing the body of this procedure into
 # a separate shell script, github can updated it even while
 # we are executing this one that is perpetually in-use.
 
